f8783c0c5c8cde865fce9e0f89c4f065c1a52113
Naveen Varma <naveenvarma9966@gmail.com>
2024-07-23 14:11:40 +0530

    Added Local storage feature

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -2,6 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
+interface Task {
+  taskName: string;
+  isCompleted: boolean;
+  isEditable: boolean;
+}
+
 @Component({
   selector: 'app-todolist',
   standalone: true,
@@ -10,11 +16,13 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './todolist.component.css',
 })
 export class TodolistComponent implements OnInit {
-  taskList = [{ taskName: 'Brush', isCompleted: false, isEditable: false }];
+  taskList: Task[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getFromLocalStorage();
+  }
 
   onSubmit(form: NgForm) {
     this.taskList.push({
@@ -22,24 +30,40 @@ export class TodolistComponent implements OnInit {
       isCompleted: false,
       isEditable: false,
     });
-
+    this.saveToLocalStorage();
     form.reset();
+  }
+
+  saveToLocalStorage() {
+    let stringJSON = JSON.stringify(this.taskList);
+    localStorage.setItem('todolist', stringJSON);
+  }
+
+  getFromLocalStorage() {
+    let itemsJSONString = localStorage.getItem('todolist');
+    if (itemsJSONString != null) {
+      this.taskList = JSON.parse(itemsJSONString);
+    }
   }
 
   onCheck(index: any) {
     this.taskList[index].isCompleted = !this.taskList[index].isCompleted;
+    this.saveToLocalStorage();
   }
 
   onDeleteTask(index: any) {
     this.taskList.splice(index, 1);
+    this.saveToLocalStorage();
   }
 
   onEditTask(index: any) {
     this.taskList[index].isEditable = true;
+    this.saveToLocalStorage();
   }
 
   onSaveTask(index: any, taskInTable: string) {
     this.taskList[index].taskName = taskInTable;
     this.taskList[index].isEditable = false;
+    this.saveToLocalStorage();
   }
 }
