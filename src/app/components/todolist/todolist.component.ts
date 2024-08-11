26fcd696f49ccec067d39b1a43904be9e576bfc8
Naveen Varma <naveenvarma9966@gmail.com>
2024-08-11 23:33:31 +0530

    Made some ui changes

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -1,26 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  inject,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ChangeDetectionStrategy, inject } from '@angular/core';
-import {
-  MatDialog,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogRef,
-  MatDialogTitle,
-} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { HeaderComponent } from '../header/header.component';
 
 interface Task {
   taskName: string;
+  taskDescription: string;
   isCompleted: boolean;
-  isEditable: boolean;
+  isNameEditable: boolean;
+  isDescriptionEditable: boolean;
 }
 
 @Component({
@@ -38,12 +37,10 @@ interface Task {
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './todolist.component.html',
-  styleUrl: './todolist.component.css',
+  styleUrls: ['./todolist.component.css'],
 })
 export class TodolistComponent implements OnInit {
   taskList: Task[] = [];
-  sortAsc: boolean = true;
-  readonly dialog = inject(MatDialog);
 
   constructor() {}
 
@@ -52,56 +49,53 @@ export class TodolistComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
+    const { taskName, taskDescription } = form.value;
     this.taskList.push({
-      taskName: form.controls['task'].value,
+      taskName,
+      taskDescription,
       isCompleted: false,
-      isEditable: false,
+      isNameEditable: false,
+      isDescriptionEditable: false,
     });
     this.saveToLocalStorage();
     form.reset();
   }
 
   saveToLocalStorage() {
-    let stringJSON = JSON.stringify(this.taskList);
+    const stringJSON = JSON.stringify(this.taskList);
     localStorage.setItem('todolist', stringJSON);
   }
 
   getFromLocalStorage() {
-    let itemsJSONString = localStorage.getItem('todolist');
-    if (itemsJSONString != null) {
+    const itemsJSONString = localStorage.getItem('todolist');
+    if (itemsJSONString) {
       this.taskList = JSON.parse(itemsJSONString);
     }
   }
 
-  onCheck(index: any) {
-    this.taskList[index].isCompleted = !this.taskList[index].isCompleted;
-    this.saveToLocalStorage();
-  }
-
-  onDeleteTask(index: any) {
+  onDeleteTask(index: number) {
     this.taskList.splice(index, 1);
     this.saveToLocalStorage();
   }
 
-  onEditTask(index: any) {
-    this.taskList[index].isEditable = true;
-    this.saveToLocalStorage();
+  onEditName(index: number): void {
+    this.taskList[index].isNameEditable = true;
   }
 
-  onSaveTask(index: any, taskInTable: string) {
-    this.taskList[index].taskName = taskInTable;
-    this.taskList[index].isEditable = false;
-    this.saveToLocalStorage();
+  onSaveName(index: number): void {
+    this.taskList[index].isNameEditable = false;
   }
 
-  onSortAsc() {
-    this.taskList.sort((a, b) => a.taskName.localeCompare(b.taskName));
-    this.saveToLocalStorage();
-    this.sortAsc = !this.sortAsc;
+  onEditDescription(index: number): void {
+    this.taskList[index].isDescriptionEditable = true;
   }
-  onSortDsc() {
-    this.taskList.reverse();
+
+  onSaveDescription(index: number): void {
+    this.taskList[index].isDescriptionEditable = false;
+  }
+
+  onCompleteTask(index: number) {
+    this.taskList[index].isCompleted = true;
     this.saveToLocalStorage();
-    this.sortAsc = !this.sortAsc;
   }
 }
